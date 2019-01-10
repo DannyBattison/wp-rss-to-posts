d027731bb205d030aecc58929ca29b2bf8c68395
Danny Battison <captain@danny.gg>
2019-01-10 22:22:39 +0000

    Add plugin file

---
+++ rss-to-posts.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Plugin Name: RSS to Posts
+	Plugin URI: https://danny.gg
+	Description: Imports posts from an RSS feed to
+	Version: 1.0
+	Author: Danny Battison
+	Author URI: https://danny.gg
+	License: GPL2
+*/
