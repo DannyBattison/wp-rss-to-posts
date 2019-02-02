6647f6c684cc7b8e9610c9cfa56095ebbd924cc9
Danny Battison <captain@danny.gg>
2019-02-02 22:27:38 +0000

    Add cron to activation hook

--- rss-to-posts.php
+++ rss-to-posts.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: RSS to Posts
 	Plugin URI: https://danny.gg
-	Description: Imports posts from an RSS feed to
+	Description: Imports posts from an RSS feed into WordPress posts.
 	Version: 1.0
 	Author: Danny Battison
 	Author URI: https://danny.gg
@@ -19,3 +19,13 @@ $rssReader->registerHooks();
 
 $admin = Admin::getInstance();
 $admin->registerHooks();
+
+register_activation_hook(__FILE__, function () {
+    if (! wp_next_scheduled (RssReader::CRON_HOOK)) {
+        wp_schedule_event(time(), 'hourly', RssReader::CRON_HOOK);
+    }
+});
+
+register_deactivation_hook(__FILE__, function () {
+    wp_clear_scheduled_hook(RssReader::CRON_HOOK);
+});
