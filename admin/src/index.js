d687c671d5de0888385017aadee29c6d2c38ff3b
Danny Battison <captain@danny.gg>
2019-02-02 22:13:57 +0000

    Remove unnecessary import

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Rss2Posts from './Rss2Posts';
 
 document.addEventListener('DOMContentLoaded', function() {
